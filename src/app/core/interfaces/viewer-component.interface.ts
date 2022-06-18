01d2e8c061fa7caf3ac38f4862cbe1857bd235be
jcuenca95 <jorcuecos@gmail.com>
2022-06-18 20:58:44 +0200

    feat(file-preview): image preview working

--- src/app/core/interfaces/viewer-component.interface.ts
+++ src/app/core/interfaces/viewer-component.interface.ts
@@ -1,3 +1,12 @@
+import { Observable } from 'rxjs';
+
 export class ViewerComponent {
   file: File | undefined;
+  fileData$ = new Observable((subscribe) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      subscribe.next(e.target?.result as string);
+    };
+    reader.readAsDataURL(this.file as Blob);
+  });
 }
--- src/app/features/file-preview/viewers/image-viewer/image-viewer.component.ts
+++ src/app/features/file-preview/viewers/image-viewer/image-viewer.component.ts
@@ -6,14 +6,4 @@ import { ViewerComponent } from 'src/app/core/interfaces/viewer-component.interf
   templateUrl: './image-viewer.component.html',
   styleUrls: ['./image-viewer.component.scss'],
 })
-export class ImageViewerComponent extends ViewerComponent implements OnInit {
-  imgSrc: FileReader['result'] = '';
-
-  ngOnInit(): void {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imgSrc = e.target?.result as string;
-    };
-    reader.readAsDataURL(this.file as Blob);
-  }
-}
+export class ImageViewerComponent extends ViewerComponent {}
