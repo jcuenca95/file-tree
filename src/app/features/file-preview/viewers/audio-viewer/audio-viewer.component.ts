1aa6ba1a9b4d6e4c17ef9238fadc26afebc4a7c1
jcuenca95 <jorcuecos@gmail.com>
2022-06-19 12:34:13 +0200

    fix(file-preview): audio preview working as expected

--- src/app/features/file-preview/viewers/audio-viewer/audio-viewer.component.ts
+++ src/app/features/file-preview/viewers/audio-viewer/audio-viewer.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 import { ViewerComponent } from '../../../../core/interfaces/viewer-component.interface';
 
 @Component({
@@ -12,6 +14,18 @@ export class AudioViewerComponent
 {
   @ViewChild('audioPlayer', { read: ElementRef })
   private audioPlayer!: ElementRef<HTMLVideoElement>;
+
+  constructor(private domSanitizer: DomSanitizer) {
+    super();
+  }
+
+  override fileData$ = new Observable((subscriber) => {
+    if (this.file) {
+      subscriber.next(
+        this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(this.file))
+      );
+    }
+  });
 
   ngAfterViewInit(): void {
     this.audioPlayer?.nativeElement.load();
